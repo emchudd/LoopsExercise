d7c9270481673be96d0f75443b8012b0a9be1b16
emchudd <eclhuddleston@gmail.com>
2021-03-07 20:27:22 -0600

    finished exercise

---
+++ loopsExercise.js
@@ -0,0 +1,57 @@
+//1
+for (i=1; i <= 7; i++){
+    console.log(i);
+}
+console.log("=========================");
+//2
+for (i = 5; i <= 25; i+=4){
+    console.log(i);
+}
+console.log("=====================");
+//3a
+const wizards = [
+    "Harry Potter",
+    "Hermione Granger",
+    "Ron Weasley"
+]
+//3b
+for (names of wizards){
+    console.log(names);
+}
+console.log("====================")
+//4a
+let harryPotterMovies = 0;
+while (harryPotterMovies <= 8){
+console.log(harryPotterMovies);
+harryPotterMovies++;
+}
+console.log("===================")
+//BONUS
+
+//5a
+const hogwartsHouses = [
+    "Gryffindor",
+    "Hufflepuff",
+    "Ravenclaw",
+    "Slytherin"
+];
+
+
+//5b
+ for (houses of hogwartsHouses){
+     for (letters of houses){
+         console.log(letters);
+     }
+ };
+
+//6a
+const quote = [
+    "Yer",
+    "A",
+    "Wizard",
+    "Harry"
+];
+
+//6b 
+
+    
